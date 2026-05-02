49a85a9d7554701c75abe5cc14575292e6feb4fd
zach340 <z.a.campbell@tcu.edu>
2026-05-02 16:33:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ Run with:  python -m streamlit run app.py
 import streamlit as st
 import streamlit.components.v1 as components
 import textwrap
+from streamlit_option_menu import option_menu
 from model_utils import *
 
 # ── Module-level callbacks — must be defined before any widgets ─────────────
